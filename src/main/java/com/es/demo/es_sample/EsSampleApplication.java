3794cdbeca03e6151e740cb5b883e9d258d04658
gahyeonkwon <zkvmflcldh0@gmail.com>
2024-03-15 19:55:43 +0900

    프로젝트 생성

---
+++ src/main/java/com/es/demo/es_sample/EsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.es.demo.es_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/es/demo/es_sample/EsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.demo.es_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
